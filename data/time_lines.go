200a43a1b104d84ff63f403bb4af4886cd454835
spy <espi8624@gmail.com>
2025-04-24 00:01:17 +0900

    Refactor TimeLineItem struct to include ReservationID for consistency with FrequentPlaceItem

--- data/time_lines.go
+++ data/time_lines.go
@@ -4,11 +4,11 @@ import "yoyaku_mate_server/models"
 
 // タイムラインデータ目録
 var timeLinesData = []models.TimeLineItem{
-	{StoreName: "日の丸美容室", TimeStamp: "2025-03-20 13:00"},
-	{StoreName: "川崎食堂", TimeStamp: "2025-03-23 11:00"},
-	{StoreName: "品川食堂", TimeStamp: "2025-03-23 17:00"},
-	{StoreName: "日本橋皮膚科", TimeStamp: "2025-03-24 17:00"},
-	{StoreName: "日本橋整形外科", TimeStamp: "2025-03-25 12:00"},
+	{ReservationID: 1, StoreName: "日の丸美容室", TimeStamp: "2025-03-20 13:00"},
+	{ReservationID: 2, StoreName: "川崎食堂", TimeStamp: "2025-03-23 11:00"},
+	{ReservationID: 3, StoreName: "品川食堂", TimeStamp: "2025-03-23 17:00"},
+	{ReservationID: 4, StoreName: "日本橋皮膚科", TimeStamp: "2025-03-24 17:00"},
+	{ReservationID: 5, StoreName: "日本橋整形外科", TimeStamp: "2025-03-25 12:00"},
 }
 
 // 全てのタイムラインデータ取得
