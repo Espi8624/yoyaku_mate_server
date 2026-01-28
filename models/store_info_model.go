a0b1c31f92039ee67b68e880ff316926d1b56578
spy <espi8624@gmail.com>
2026-01-28 22:56:36 +0900

    住所入力機能改善

--- models/store_info_model.go
+++ models/store_info_model.go
@@ -11,6 +11,10 @@ type Store struct {
 	ID            primitive.ObjectID `bson:"_id,omitempty" json:"_id"`
 	StoreName     string             `bson:"store_name" json:"store_name"`
 	Address       string             `bson:"address" json:"address"`
+	Building      string             `bson:"building" json:"building"`
+	ZipCode       string             `bson:"zip_code,omitempty" json:"zip_code,omitempty"`
+	Prefecture    string             `bson:"prefecture,omitempty" json:"prefecture,omitempty"`
+	City          string             `bson:"city,omitempty" json:"city,omitempty"`
 	Phone         string             `bson:"phone" json:"phone"`
 	UserID        primitive.ObjectID `bson:"user_id" json:"user_id"`
 	StoreID       string             `bson:"store_id" json:"store_id"`
